731d28b44190ae5315a3a176b97dc7668d6d290f
EleazarGomez <eliaaron.gomez@gmail.com>
2019-04-23 19:29:12 -0700

    indentation

--- src/constants.h
+++ src/constants.h
@@ -9,11 +9,11 @@
 const int NUM_PROCESSES = 5;
 
 const char LOWER_CASE_LETTERS[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                                     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-						 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-					       'y', 'z'};
+						 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+						 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+						 'y', 'z'};
 
-const char UPPER_CASE_LETTERS[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+const char UPPER_CASE_LETTERS[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 						 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
 						 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
 						 'Y', 'Z'};
